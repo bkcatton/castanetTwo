c8b9d6ee7112657678603175cbd0d615929b0c97
Casey <caseyjtite@gmail.com>
2022-02-12 10:07:07 -0800

    working on querys

--- routes/listings.js
+++ routes/listings.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM listings;`)
-      .then(data => {
+      .then((data) => {
         // console.log(data);
         const templateVars = data.rows;
         res.render("listings", { templateVars });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
