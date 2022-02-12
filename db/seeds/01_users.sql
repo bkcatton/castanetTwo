c1bf09587ecede71723d6361da33f55cfa9db3af
Matheson94 <mathesonchisholm@hotmail.com>
2022-02-12 02:10:15 +0000

    DB Fix

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,13 +1,12 @@
 -- Users table seeds here (Example)
 INSERT INTO users (id, name, phone_number, is_admin)
 VALUES
-(1, Matt, 2501233456, true),
-(2, Casey, 2501233156, true),
-(1, Ben, 2571233456, true),
-(1, John, 2501244456, false),
-(1, Peter, 2502233226, false),
-(1, Paul, 2501243226, false),
-(1, Sam, 2501234226, false),
-(1, Kerry, 2501233226, false),
-(1, Kerry, 2501233227, false);
+(1, 'Matt', 2501233456, true),
+(2, 'Casey', 2501233156, true),
+(3, 'Ben', 2571233456, true),
+(4, 'John', 2501244456, false),
+(5, 'Peter', 2502233226, false),
+(6, 'Paul', 2501243226, false),
+(7, 'Sam', 2501234226, false),
+(8, 'Kerry', 2501233226, false);
 
--- db/seeds/04_favorites.sql
+++ db/seeds/04_favorites.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO favorites (id, user_id, listing_id)
 VALUES
-(1, 1, 2,),
+(1, 1, 2),
 (2, 2, 3),
 (3, 4, 1);
