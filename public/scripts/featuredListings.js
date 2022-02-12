ab77edf3ff1871267d15648e08a62815eaa7e03d
Matheson94 <mathesonchisholm@hotmail.com>
2022-02-12 23:33:55 +0000

    home page rendering

--- public/scripts/featuredListings.js
+++ public/scripts/featuredListings.js
@@ -1,24 +1,39 @@
+
+
 // Client facing scripts here
 $(document).ready(function () {
-// const $change = $('#target');
-
-const $topRow = $('.top-row');
-const $addData = function (data) {
-  $topRow.append(`<p>${dataUser}</p>`);
-}
-$topRow.append('<p>Hello</p>');
-  $.ajax({
-    url: 'http://localhost:8080/api/featuredListings',
-    method: 'GET',
-    dataType: 'json',
-    success: (data) => {
-      const dataUser = data['users'][0];
-      $topRow.append(`<p>${dataUser.name}</p>`);
-      console.log('this request succeeded and here\'s the data', data['users'][0]);
-    },
-    error: (error) => {
-      console.log('this request failed and this was the error', error);
+  // // const $change = $('#target');
+  const $topRow = $('.top-row');
+  const $addListing = function (listing) {
+    console.log(listing);
+    const $listingContainer = `<article>
+  <img src='${listing.photo_url}' />
+  <h3>${listing.title}</h3>
+  <h3>${listing.city}</h3>
+  <h3>${listing.price}</h3>
+  </article>`;
+    return $listingContainer;
+  }
+  const renderListing = function (listings) {
+    const array = listings.listings;
+    for (let listing of array) {
+      const $item = $addListing(listing);
+      $('.top-row').append($item);
     }
-  });
-});
-
+  }
+  const loadListings = function () {
+    $.ajax({
+      url: 'http://localhost:8080/api/featuredListings',
+      method: 'GET',
+      dataType: 'json',
+      success: (data) => {
+        renderListing(data)
+        // console.log(listings);
+      },
+      error: (error) => {
+        console.log('this request failed and this was the error', error);
+      }
+    })
+  }
+  loadListings();
+})
