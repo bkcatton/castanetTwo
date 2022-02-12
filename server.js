2f42deb0691113a9cdd5b9076767ba4c89da4e57
Matheson94 <mathesonchisholm@hotmail.com>
2022-02-12 17:07:11 +0000

    / shows listings and css

--- server.js
+++ server.js
@@ -46,15 +46,16 @@ const listingsRoutes = require('./routes/listings')
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/listings", listingsRoutes(db));
+app.use("/", listingsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// app.get("/", (req, res) => {
+//   res.render("listings");
+// });
 // app.get("/home", (req, res) => {
 //   res.render("home");
 // });
