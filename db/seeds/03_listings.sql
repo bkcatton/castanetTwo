a5b3ca963d826d7261165ff633b46a82c560ff5c
Matheson94 <mathesonchisholm@hotmail.com>
2022-02-12 01:23:34 +0000

    listings seeds

--- db/seeds/03_listings.sql
+++ db/seeds/03_listings.sql
@@ -1,4 +1,4 @@
-INSERT INTO listings listings (id, title, street_name_number, city, postal_code, sq_ft, seller_id, isActive,
+INSERT INTO listings (id, title, street_name_number, city, postal_code, sq_ft, seller_id, isActive,
   property_type,
   bedroom_number,
   bathroom_number,
@@ -6,3 +6,7 @@ INSERT INTO listings listings (id, title, street_name_number, city, postal_code,
   photo_url,
   price
 )
+VALUES
+(1, 'firsthouse', '123 hello ave', 'Victoria', '7y63j2', 1200, 2, true, 'house', 3, 3, 3, 'https://media.istockphoto.com/photos/beautiful-residential-home-exterior-on-bright-sunny-day-with-green-picture-id1211174464?k=20&m=1211174464&s=612x612&w=0&h=fQ3ahmaJnYcZb0UQtBXvOhcuhHFTgK9BA5Mylic7Gnw=', $1000000)
+(2, 'secondhouse', '789 world ave', 'Victoria', '7y63j2', 2800, 4, true, 'house', 2, 4, 6, 'https://media.istockphoto.com/photos/beautiful-residential-home-exterior-on-bright-sunny-day-with-green-picture-id1211174464?k=20&m=1211174464&s=612x612&w=0&h=fQ3ahmaJnYcZb0UQtBXvOhcuhHFTgK9BA5Mylic7Gnw=', $400000)
+(3, 'thirdhouse', '444 johnson st', 'Victoria', '7y63j2', 3800, 1, true, 'house', 2, 5, 5, 'https://media.istockphoto.com/photos/beautiful-residential-home-exterior-on-bright-sunny-day-with-green-picture-id1211174464?k=20&m=1211174464&s=612x612&w=0&h=fQ3ahmaJnYcZb0UQtBXvOhcuhHFTgK9BA5Mylic7Gnw=', $800000)
