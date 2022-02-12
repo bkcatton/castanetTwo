80a35f5ce62e591fcfc42f37ca8eb54be0287819
Casey <caseyjtite@gmail.com>
2022-02-12 10:29:12 -0800

    work on listings

--- routes/listings.js
+++ routes/listings.js
@@ -13,5 +13,24 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
+  router.post("/", (req, res) => {
+    console.log(req.body.city);
+
+    db.query(
+      `SELECT * FROM listings
+    WHERE city = $1
+    AND price < $2;`,
+      [req.body.city, req.body.price]
+    )
+      .then((data) => {
+        // console.log(data);
+        const templateVars = data.rows;
+        res.render("listings", { templateVars });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+    return;
+  });
   return router;
 };
