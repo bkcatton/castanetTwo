6527c2ca0d517d82723b0cdc4b0913ed969fcd9b
Matheson94 <mathesonchisholm@hotmail.com>
2022-02-14 01:50:23 +0000

    db fixes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,16 +1,22 @@
 -- Users table seeds here (Example)
 INSERT INTO users (id, name, phone_number, is_admin)
 VALUES
-(1, 'Matt', 2501233456, true),
-(2, 'Casey', 2501233156, true),
-(3, 'Ben', 2571233456, true),
-(4, 'John', 2501244456, false),
-(5, 'Peter', 2502233226, false),
-(6, 'Paul', 2501243226, false),
-(7, 'Sam', 2501234226, false),
-(8, 'Kerry', 2501233226, false),
-(9, 'Terry', 7802233226, false),
-(10, 'Christian', 4031243226, false),
-(11, 'Felipe', 6661234226, false),
-(12, 'Grant', 7801233226, false);
+(1, 'Matt C', 2501233456, true),
+(2, 'Casey T', 2501233156, true),
+(3, 'Ben C', 2571233456, true),
+(4, 'John G', 2501244456, false),
+(5, 'Peter F', 2502233226, false),
+(6, 'Paul X', 2501243226, false),
+(7, 'Sam H', 2501234226, false),
+(8, 'Kerry K', 2501233226, false),
+(9, 'Terry L', 7802233226, false),
+(10, 'Christian P', 4031243226, false),
+(11, 'Felipe G', 6661234226, false),
+(12, 'Grant R', 7801233226, false),
+(13, 'Mohammed K', 7802233226, false),
+(14, 'Neil A', 4031243226, false),
+(15, 'Xavier W', 6661234226, false),
+(16, 'Jessica T', 7802233226, false),
+(17, 'Christian M', 4031243226, false),
+(18, 'Violet W', 6661234226, false);
 
