dca4e5e411edd475d85f3ec9cb614696069a51a4
Casey <caseyjtite@gmail.com>
2022-02-15 16:31:04 -0800

    add class to button

--- public/scripts/favorites.js
+++ public/scripts/favorites.js
@@ -53,7 +53,7 @@ const $createFavorite = function (listing) {
 <h3 class="city">${listing.city}</h3>
 <h3 class="price">$${listing.price}</h3>
 
-<button class="fave" id="${listing.fid}">Unfavorite</button>
+<button class="fave unfave" id="${listing.fid}">Unfavorite</button>
 
 </article>`;
 
