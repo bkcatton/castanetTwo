73bf895d7b5d15ee421a680687971945fe6565ef
Matheson94 <mathesonchisholm@hotmail.com>
2022-02-16 20:06:01 +0000

    small css tweaks

--- routes/featuredListingsQ.js
+++ routes/featuredListingsQ.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM listings;`)
+    db.query(`SELECT * FROM listings ORDER BY RANDOM () LIMIT 6;`)
       .then((data) => {
         const listings = data.rows;
         res.json({ listings });
